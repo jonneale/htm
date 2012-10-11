9eb1f7c19321ae010bf0f37de303a8e49b196790
Jon Neale <jon.neale@uswitch.com>
2012-10-11 15:32:13 +0100

    Sparse representation should work but doesnt

--- src/htm/core.clj
+++ src/htm/core.clj
@@ -9,14 +9,15 @@
 (def off
      "off")
 
-(defn create-cell
+(defn create-synapse
   [state input]
   {:state state
-   :input input})
+   :input input
+   :permanence 1})
 
 (defn column
   [size]
-  (reduce #(into %1 [(create-cell off %2)]) []  (range 1 (inc size))))
+  {:boost 1 :synapses (reduce #(into %1 [(create-synapse off %2)]) []  (range 1 (inc size)))})
 
 (def region
   (for [i (range 1 5)]
@@ -31,19 +32,29 @@
   (not (on? value)))
 
 (def permanence-threshold
-  10)
+  0.5)
 
 ;;if set to 2, then 2% of input will remain activated
 (def sparse-representation-percentage
   10)
 
+(def min-overlap
+  10)
+
 (defn calculate-overlap
-  [data model]
-  )
+  [region input]
+  (map (fn [{:keys [boost synapses]}]
+         (println "here")
+
+
+         (let [connected-synapses (filter #(> (:permanence %) permanence-threshold) synapses)
+               overlap (reduce #(+ %1 (input (:input %2))) 0 connected-synapses)
+               weighted-overlap (if (> overlap min-overlap) (* boost overlap) 0)]
+           (assoc column :overlap weighted-overlap)))
+       region))
 
 (defn create-sparse-representation
   {:doc "Input is expected to have been processed into a vector the same length as the single region"}
   [input]
-  (let [connected-synapses (filter #(> (:permanance %) permanence-threshold) region)
-        overlap (calculate-overlap connected-synapses )
-        ]))
+  (let [overlap (calculate-overlap region input)]
+    overlap))
