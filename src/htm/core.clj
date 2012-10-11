f41ff22a07df4ed2293b37e790ffb2e2a86823e9
Jon Neale <jon.neale@uswitch.com>
2012-10-11 14:57:38 +0100

    Now specifying size of regions

--- src/htm/core.clj
+++ src/htm/core.clj
@@ -4,3 +4,46 @@
   "I don't do a whole lot."
   [& args]
   (println "Hello, World!"))
+
+
+(def off
+     "off")
+
+(defn create-cell
+  [state input]
+  {:state state
+   :input input})
+
+(defn column
+  [size]
+  (reduce #(into %1 [(create-cell off %2)]) []  (range 1 (inc size))))
+
+(def region
+  (for [i (range 1 5)]
+    (column 4)))
+
+(defn on?
+  [value]
+  (= 1 value))
+
+(defn off?
+  [value]
+  (not (on? value)))
+
+(def permanence-threshold
+  10)
+
+;;if set to 2, then 2% of input will remain activated
+(def sparse-representation-percentage
+  10)
+
+(defn calculate-overlap
+  [data model]
+  )
+
+(defn create-sparse-representation
+  {:doc "Input is expected to have been processed into a vector the same length as the single region"}
+  [input]
+  (let [connected-synapses (filter #(> (:permanance %) permanence-threshold) region)
+        overlap (calculate-overlap connected-synapses )
+        ]))
