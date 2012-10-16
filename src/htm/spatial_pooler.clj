af4fd805adff487eec3adf9bec959abd232bd858
Jon Neale <jon.neale@uswitch.com>
2012-10-16 19:10:14 +0100

    Updated bugs in spatial pooler preventing very small network being created

--- src/htm/spatial_pooler.clj
+++ src/htm/spatial_pooler.clj
@@ -1,19 +1,24 @@
 (ns htm.spatial-pooler
   (:use [htm.config.constants]))
 
-(defn- on?
+(defn on?
   [synapse]
   (= on (:state synapse)))
 
-(defn- off?
+(defn off?
   [synapse]
   (not (on? synapse)))
 
-(defn- random-permanence
+(defn random-permanence
   []
   (+ (rand permanence-threshold) (/ permanence-threshold 2)))
 
-(defn- create-cell
+(defn region
+  [cells-in-column number-of-columns]
+  (let [region (map #(column cells-in-column %) (range 1 (inc number-of-columns)))]
+    (add-segments-to-cells region)))
+
+(defn create-cell
   [position column]
   {:lateral-synapses []
    :predictive-state off
@@ -21,13 +26,13 @@
    :column column
    :position position})
 
-(defn- create-synapse
+(defn create-synapse
   [state input]
   {:state state
    :input (dec input)
    :permanence (random-permanence)})
 
-(defn- create-lateral-synapse
+(defn create-lateral-synapse
   [from-cell to-cell]
   {:from-column (:column from-cell)
    :from-cell (:position from-cell)
@@ -35,26 +40,25 @@
    :to-cell (:column to-cell)
    :state off})
 
-(defn- column
+(defn column
   [size position]
   {:boost (int (rand 10))
    :position position
    :active false
    :cells (reduce #(into %1 [(create-cell %2 position)]) []
-                     (range 1 (inc cells-per-column)))
+                     (range 1 (inc size)))
    :synapses (reduce #(into %1 [(create-synapse off %2)]) []
-                     (take (int (rand size))
-                           (sort-by rand (range 1 (inc size)))))})
-
+                     (take (max (int (rand size)) 1)
+                                   (sort-by rand (range 1 (inc size)))))})
 (defn pick-random-cell
   [region]
-  (let [column (nth region (rand (dec (count region))))]
-    (nth (:cells column) (rand (dec (count column))))))
+  (let [column (nth region (rand (count region)))
+        random (min (int (rand (count (:cells column)))) (dec (count (:cells column))))]
+    (nth (:cells column) random)))
 
 (defn add-lateral-synapses
   [cell region]
-  (reduce (fn [agg _] (into agg [(create-lateral-synapse cell (pick-random-cell region))])) [] (range 1 (inc synapses-per-segment)))
-  )
+  (reduce (fn [agg _] (into agg [(create-lateral-synapse cell (pick-random-cell region))])) [] (range 1 (inc synapses-per-segment))))
 
 
 (defn add-segments
@@ -68,12 +72,7 @@
          (assoc column :cells (map #(add-segments % region) cells)))
        region))
 
-(defn region
-  []
-  (let [region (map #(column 4 %) (range 1 5))]
-    (add-segments-to-cells region)))
-
-(defn- update-column-states
+(defn update-column-states
   [{:keys [synapses] :as column}]
   (assoc column :synapses
          (map #(if (> (:permanence %) permanence-threshold)
@@ -81,7 +80,7 @@
                                    %)
                                 synapses)))
 
-(defn- calculate-overlap
+(defn calculate-overlap
   {:doc "Takes the overlapping values of each of the columns and calculates the number of inputs connected to each column via an active synapse (a synapse associated with the column with a permanance value above the permanence threshold)"}
   [region input]
   (map (fn [{:keys [boost synapses] :as column}]
@@ -91,7 +90,7 @@
            (assoc column :overlap weighted-overlap)))
        region))
 
-(defn- neighbours
+(defn neighbours
   [column region]
   (filter #(or (= (:position %) (inc (:position column))) (= (:position %) (dec (:position column)))) region))
 
@@ -100,15 +99,17 @@
   (let [column-neighbours (neighbours column region)
         sorted-neighbours (sort-by :overlap column-neighbours)
         position (dec (min (count sorted-neighbours) k))]
-    (:overlap (nth sorted-neighbours position))))
+    (:overlap (first sorted-neighbours))))
 
-(defn- inhibit
+(defn inhibit
   [column all-columns]
   (let [local-kth-score (kth-score column all-columns desired-local-activity)]
     (when (> (:overlap column) local-kth-score) (> (:overlap column) 0)
           column)))
 
-(defn- activate-columns
+
+;;TODO - there can currently be no active columns
+(defn activate-columns
   [columns]
   (map (fn [column]
          (assoc column :active
@@ -126,12 +127,15 @@
                   (assoc synapse :permanence (operator permanence permanence-inc))))
               synapses)))
 
+
+
 (defn create-sparse-representation
-  {:doc "Input is expected to have been processed into a vector the same length as the single region"}
+  {:doc "Input can be any size - output of this function is a list of active columns (the list of columns that win due to bottom up input at time t) which should then be used as input to the temporal pooler"}
   [input]
-  (let [updated-region (map update-column-states (region))
+  (let [;;region (initialise-region input columns-in-region)
+        updated-region (map update-column-states (region cells-per-column columns-in-region))
         overlap (calculate-overlap updated-region input)
-        activated-columns (activate-columns overlap)
+        activated-columns (filter (comp true? :active) (activate-columns overlap))
         updated-permanence-columns (map update-synapse-permanence activated-columns)]
     {:active-columns updated-permanence-columns
      :region overlap}))
